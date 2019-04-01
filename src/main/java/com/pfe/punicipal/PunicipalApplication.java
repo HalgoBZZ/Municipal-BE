eae27a9771e3c32cc2d3ea23dbb9ed67908f2bdb
HalgoBZZ <hafedh.baazouzi@gmail.com>
2019-04-01 21:28:23 +0200

    first commit

---
+++ src/main/java/com/pfe/punicipal/PunicipalApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.punicipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunicipalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunicipalApplication.class, args);
+	}
+
+}
